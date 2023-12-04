9fdc743b8f390fcc9c4984725d1471e2f28d145c
Victor Rotha <victor.rotha@t-online.de>
2023-12-04 13:16:56 +0100

    day 04 part 02

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -8,31 +8,44 @@ fun main() {
 
     val file = File("src/main/kotlin/day04/input.txt")
 
-    partOne(file)
+    solutions(file)
 
 }
 
-fun partOne(file: File) {
+fun solutions(file: File) {
 
-    var sum = 0
+    var partOneSum = 0
+    val cards : MutableMap<Int, Int> = mutableMapOf()
 
     file.bufferedReader().forEachLine { line ->
 
+        val id = line.split(":")[0].split(Regex(" +"))[1].toInt()
         val numbers = line.split(":")[1].split("|").map{ it.trim() }
-
         val target = numbers[0].split(Regex(" +"))
         val yourNumbers = numbers[1].split(Regex(" +"))
+
+        cards.putIfAbsent(id, 0)
+        cards[id] = cards[id]!! + 1
 
         val n = target.intersect(yourNumbers.toSet()).size
         if (n > 0) {
             val points = 2.0.pow((n - 1).toDouble()).toInt()
-            sum += points
+            partOneSum += points
+
+            for (i in id + 1 .. id + n) {
+                cards.putIfAbsent(i, 0)
+                cards[i] = cards[i]!! + cards[id]!!
+            }
 
         }
 
     }
 
-    println("Part One: $sum ")
+    println("Part One: $partOneSum ")
+
+    val partTwoSum = cards.map{ it.value }.sum()
+    println("Part Two: $partTwoSum")
 
 
 }
+
