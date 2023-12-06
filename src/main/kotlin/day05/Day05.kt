058e49282c47886e97f712464c9fbd69904e1035
Victor Rotha <victor.rotha@t-online.de>
2023-12-06 17:22:45 +0100

    combined day 05 parts 01 and 02

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -7,19 +7,17 @@ import kotlin.math.min
 
 fun main() {
 
-
     val file = File("src/main/kotlin/day05/input.txt")
 
-    partOne(file)
-    partTwo(file)
+    solutions(file)
 
 
 }
 
-
-fun partOne(file: File) {
+fun solutions(file: File) {
 
     var seeds: List<Long> = listOf()
+    val seedPairs: MutableList<Pair<Long, Long>> = mutableListOf()
 
     val maps: List<MutableList<List<Long>>> = listOf(
         mutableListOf(),
@@ -32,7 +30,7 @@ fun partOne(file: File) {
     )
 
     var mapIdx: Int = -1
-    var minResult = -1L
+
 
     file.bufferedReader().forEachLine { line ->
 
@@ -40,87 +38,9 @@ fun partOne(file: File) {
             return@forEachLine
 
         if (line.startsWith("seeds:")) {
+
             seeds = line.split(":")[1].trim().split(" ").map { it.toLong() }
-            return@forEachLine
-        }
 
-        if (line.contains("-"))
-            mapIdx++
-        else {
-            val commands = line.trim().split(" ").map{it.toLong()}
-            maps[mapIdx].add(commands)
-        }
-
-    }
-    for (seed in seeds) {
-        var result = seed
-
-        for (map in maps)
-            result = mapInput(result, map)
-
-        minResult = if (minResult == -1L)
-            result
-        else
-            min(minResult, result)
-
-    }
-
-    println("Part One: $minResult")
-
-}
-
-fun mapInput(input: Long, maps : List<List<Long>>) : Long {
-
-    for (commands in maps) {
-        if (input in commands[1]..commands[1] + commands[2]) {
-            val diff = input - commands[1]
-            return commands[0] + diff
-
-        }
-    }
-
-    return input
-}
-
-fun mapInputReversed(target: Long, maps : List<List<Long>>) : Long {
-
-for (commands in maps) {
-    if (target in commands[0]..< commands[0] + commands[2]) {
-        val diff = target - commands[0]
-        return commands[1] + diff
-
-    }
-}
-
-return target
-}
-
-
-fun partTwo(file: File) {
-
-    var seedPairs: MutableList<Pair<Long, Long>> = mutableListOf()
-
-    val maps: List<MutableList<List<Long>>> = listOf(
-        mutableListOf(),
-        mutableListOf(),
-        mutableListOf(),
-        mutableListOf(),
-        mutableListOf(),
-        mutableListOf(),
-        mutableListOf(),
-    )
-
-    var mapIdx: Int = -1
-
-
-    file.bufferedReader().forEachLine { line ->
-
-        if (line.isEmpty())
-            return@forEachLine
-
-        if (line.startsWith("seeds:")) {
-
-            val seeds = line.split(":")[1].trim().split(" ").map { it.toLong() }
             var startSeed : Long = -1L
             for (i  in seeds.indices) {
                 if (i.mod(2) == 0) {
@@ -144,20 +64,46 @@ fun partTwo(file: File) {
 
     }
 
+    processSeeds(seeds, maps)
+
     processReversed(seedPairs, maps)
 
+}
+
+fun mapInput(input: Long, maps : List<List<Long>>) : Long {
+
+    for (commands in maps) {
+        if (input in commands[1]..commands[1] + commands[2]) {
+            val diff = input - commands[1]
+            return commands[0] + diff
+
+        }
     }
+
+    return input
+}
+
+fun mapInputReversed(target: Long, maps : List<List<Long>>) : Long {
+
+for (commands in maps) {
+    if (target in commands[0]..< commands[0] + commands[2]) {
+        val diff = target - commands[0]
+        return commands[1] + diff
+    }
+}
+
+return target
+}
+
 
 
 fun processReversed(seeds : MutableList<Pair<Long, Long>>, maps : List<MutableList<List<Long>>>) {
 
-    var result = 0L
+    var result: Long
 
     var location = 0L
 
-    val maxTries = Long.MAX_VALUE
-
-    while (location < maxTries) {
+    while (location < Long.MAX_VALUE) {
 
         result = location
 
@@ -189,37 +135,24 @@ fun isInSeeds(target : Long, seeds : List<Pair<Long, Long>>) : Boolean {
 
 
 
-fun processSeeds(seeds : MutableList<Pair<Long, Long>>, maps : List<MutableList<List<Long>>>) {
-var minResult = -1L
+fun processSeeds(seeds : List<Long>, maps : List<MutableList<List<Long>>>) {
+    var minResult : Long = -1
 
     for (seed in seeds) {
+        var result = seed
 
-        for (i in seed.first..<seed.first + seed.second) {
+        for (map in maps)
+            result = mapInput(result, map)
 
-
-            var result = i
-
-            for (map in maps)
-                result = mapInput(result, map)
-
-            minResult = if (minResult == -1L) {
-                println("first value $result")
-                result
-            } else {
-
-                val lowest = min(minResult, result)
-                if (lowest != minResult)
-                    println("lowest: $lowest")
-                lowest
-
-            }
-
-        }
-
+        minResult = if (minResult == -1L)
+            result
+        else
+            min(minResult, result)
 
     }
 
     println("Part One: $minResult")
+
 
 }
 
